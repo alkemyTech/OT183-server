2de7cc6eba833ed8a42026a6e34d749efbf2822f
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-04-14 14:33:40 -0300

    Annotation '@repository' added

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
