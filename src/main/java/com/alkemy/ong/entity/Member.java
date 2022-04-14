4b8f2b515a72dd4c28210e1fb79c7ee95b8c30b9
Julian Greco <julian_greco@hotmail.com>
2022-04-14 12:14:12 -0300

    Member Entity and Repository added.

---
+++ src/main/java/com/alkemy/ong/entity/Member.java
@@ -0,0 +1,49 @@
+package com.alkemy.ong.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE member SET deleted = true WHERE id =?")
+@Where(clause = "deleted = false")
+@Data
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String facebookUrl;
+
+    private String instagramUrl;
+
+    private String linkedinUrl;
+
+    @Column(nullable = false)
+    private String image;
+
+    private String description;
+
+    @CreationTimestamp
+    private LocalDate created;
+
+    @UpdateTimestamp
+    private LocalDate updated;
+
+    private boolean deleted;
+
+}
+
