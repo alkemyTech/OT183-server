183744e4e444bebd0d990ab78c1b4dabdae1acae
lucasbravi2019 <lucas.bravi.2008@gmail.com>
2022-04-14 07:33:02 -0300

    OT183-16 Role model and repository added

---
+++ src/main/java/com/alkemy/ong/role/Role.java
@@ -0,0 +1,38 @@
+package com.alkemy.ong.role;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+
+}
---
+++ src/main/java/com/alkemy/ong/role/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
