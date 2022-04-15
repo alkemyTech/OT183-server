f2ab77018ec75a23059a1a24a3bbd035bacc310f
Celso Zabala <celsobrc@gmail.com>
2022-04-15 20:33:30 -0300

    Activity entity renamed

--- src/main/java/com/alkemy/ong/model/Activity.java
+++ src/main/java/com/alkemy/ong/model/Activity.java
@@ -19,7 +19,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ActivityEntity {
+public class Activity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/alkemy/ong/repository/ActivityRepository.java
+++ src/main/java/com/alkemy/ong/repository/ActivityRepository.java
@@ -1,9 +1,9 @@
 package com.alkemy.ong.repository;
 
-import com.alkemy.ong.model.ActivityEntity;
+import com.alkemy.ong.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ActivityRepository extends JpaRepository<ActivityEntity, Long> {
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
 }
