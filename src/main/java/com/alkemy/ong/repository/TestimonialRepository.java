2062022feea86c5360e7062878170cdc35153a13
mary-puma <pumaisabel029@gmail.com>
2022-04-17 17:58:44 -0300

    fixes: styles and format

--- src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
@@ -1,6 +1,6 @@
 package com.alkemy.ong.repository;
 
-import com.alkemy.ong.entity.Testimonial;
+import com.alkemy.ong.model.Testimonial;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
