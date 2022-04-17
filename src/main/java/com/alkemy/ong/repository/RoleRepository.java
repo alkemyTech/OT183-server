9b75b983d7f9f371a05a101db30088b613a6753a
lucasbravi2019 <80082740+lucasbravi2019@users.noreply.github.com>
2022-04-16 21:59:11 -0300

    Feature/ot183-16 (#1)
    * OT183-16 Role model, repository and validation added

---
+++ src/main/java/com/alkemy/ong/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
