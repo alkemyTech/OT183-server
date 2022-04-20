2957cd272f8352f72c2dc4bc8e713b8b1e83ec04
mary-puma <pumaisabel029@gmail.com>
2022-04-20 16:16:23 -0300

    Annotation email added

--- src/main/java/com/alkemy/ong/dto/LoginDto.java
+++ src/main/java/com/alkemy/ong/dto/LoginDto.java
@@ -3,9 +3,13 @@ package com.alkemy.ong.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+
 @Setter
 @Getter
 public class LoginDto {
+
+    @Email
     private String email;
     private String password;
 }
