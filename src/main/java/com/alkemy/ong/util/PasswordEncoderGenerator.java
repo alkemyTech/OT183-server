a914259706bb7ab6ae12a03c6a4857cb03de6761
mary-puma <pumaisabel029@gmail.com>
2022-04-20 16:13:43 -0300

    to encrypt

---
+++ src/main/java/com/alkemy/ong/util/PasswordEncoderGenerator.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderGenerator {
+    public static void main(String[] args) {
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("hola"));
+
+    }
+}
