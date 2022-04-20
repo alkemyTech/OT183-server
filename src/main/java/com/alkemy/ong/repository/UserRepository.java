1922a72d8d78fc243ceeceedcc57bcff8b63ecbd
mary-puma <pumaisabel029@gmail.com>
2022-04-20 16:02:50 -0300

    Annotation ManyToMany added

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.alkemy.ong.repository;
 
-import com.alkemy.ong.model.User;
+import com.alkemy.ong.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+    Optional<UserModel> findByEmail(String email);
 }
