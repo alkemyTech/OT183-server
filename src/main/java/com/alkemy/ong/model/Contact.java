5e8888985e4e6b4e77b653960c9f94fa82822410
Celso Zabala <celsobrc@gmail.com>
2022-04-21 13:25:20 -0300

    Contact entity added and updated messages.properties

---
+++ src/main/java/com/alkemy/ong/model/Contact.java
@@ -0,0 +1,52 @@
+package com.alkemy.ong.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="contacts")
+@SQLDelete(sql = "UPDATE contacts SET deleted=true WHERE id=?")
+@Where(clause = "deleted=false")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "{name.null}")
+    private String name;
+
+    @NotNull(message = "{phone.null}")
+    private String phone;
+
+    @NotNull(message = "{email.blank}")
+    @Email(message = "{email.format}")
+    private String email;
+
+    @NotNull(message = "{message.null}")
+    @Column(columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "deleted")
+    private boolean deleteAt = Boolean.FALSE;
+
+    @CreationTimestamp
+    private LocalDate created;
+
+    @UpdateTimestamp
+    private LocalDate updated;
+
+}
