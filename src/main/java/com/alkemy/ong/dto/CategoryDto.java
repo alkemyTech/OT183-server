6ffd4092ba4214497273e18b7a224ad86e82e4b2
Julian Greco <julian_greco@hotmail.com>
2022-04-21 21:03:08 -0300

    CategoryDto added

---
+++ src/main/java/com/alkemy/ong/dto/CategoryDto.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+
+    private Long id;
+
+    @NotNull(message = "Name can't be null")
+    private String name;
+
+    private String description;
+
+    private String image;
+
+    private String created;
+
+    private String updated;
+
+
+}
--- src/main/java/com/alkemy/ong/model/Organization.java
+++ src/main/java/com/alkemy/ong/model/Organization.java
@@ -1,5 +1,5 @@
-package com.alkemy.ong.model;
-
+//package com.alkemy.ong.model;
+/*
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
@@ -66,3 +66,73 @@ public class Organization {
  * Use @FilterDef and @Filter above the class definition to make the
  * soft deleted data accesible to, for example, an administrator.
  * */
+package com.alkemy.ong.model;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "organizations")
+@SQLDelete(sql = "UPDATE table_product SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
+public class Organization {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Name must not be empty")
+	@Column(name="Name")
+	private String name;
+
+	@Column(name="Image")
+	private String image;
+
+	@Column(name="Address")
+	private String address;
+
+	@Column(name="Phone")
+	private int phone;
+
+	@NotBlank(message = "Email must not be empty")
+	@Column(name="Email")
+	private String email;
+
+	@Column(name="WelcomeText")
+	private String welcomeText;
+
+	@Column(name="AboutUsText")
+	private String aboutUsText;
+
+	@Column(updatable = false)
+	@CreationTimestamp
+	private LocalDate created;
+
+	@UpdateTimestamp
+	private LocalDate updated;
+
+	private boolean deleted = Boolean.FALSE;
+}
+
+/* Use @FilterDef and @Filter above the class definition to make the
+ * soft deleted data accesible to, for example, an administrator.
+		 * */
