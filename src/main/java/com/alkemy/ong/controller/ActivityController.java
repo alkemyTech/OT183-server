334847b0cd78089f460266a22eaeb7b9c8d2ab84
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-04-23 18:56:08 -0300

    Activity controller added

---
+++ src/main/java/com/alkemy/ong/controller/ActivityController.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.dto.ActivityDto;
+import com.alkemy.ong.model.Activity;
+import com.alkemy.ong.service.impl.ActivityServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/activities")
+public class ActivityController {
+
+    @Autowired
+    private ActivityServiceImpl service;
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ActivityDto> createTestimonial(@Valid @RequestBody ActivityDto activityDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createActivity(activityDto));
+    }
+}
