f628f334d4b170717a61f1b01ccb15f09c3d0b70
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-04-23 23:00:38 -0300

    NewsController added

---
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -0,0 +1,33 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.service.impl.NewsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Locale;
+
+@RestController
+@RequestMapping("/news")
+public class NewsController {
+
+    @Autowired
+    private NewsServiceImpl service;
+
+    @Autowired
+    private MessageSource message;
+
+    @GetMapping("/{id}")
+    //@PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getNewsById(@PathVariable("id") long id) throws EntityNotFoundException {
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message.getMessage("data.not.found", null, Locale.US));
+        }
+
+    }
+}
