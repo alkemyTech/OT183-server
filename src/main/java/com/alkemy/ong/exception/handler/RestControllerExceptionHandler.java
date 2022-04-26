0511b990cb04bdbc9ef456337e51947bfb3cd099
Julian Greco <julian_greco@hotmail.com>
2022-04-26 19:44:19 -0300

    NullModelExeption Handler fixed

--- src/main/java/com/alkemy/ong/exception/handler/RestControllerExceptionHandler.java
+++ src/main/java/com/alkemy/ong/exception/handler/RestControllerExceptionHandler.java
@@ -103,4 +103,15 @@ public class RestControllerExceptionHandler extends ResponseEntityExceptionHandl
         );
     }
 
+    @ExceptionHandler(NullModelException.class)
+    public ResponseEntity<ApiErrorResponse<String>> handleDataRepresentationException(NullModelException ex) {
+        return new ResponseEntity<>(
+                new ApiErrorResponse<>(
+                        HttpStatus.NOT_FOUND,
+                        ex.getMessage()
+                ),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
 }
