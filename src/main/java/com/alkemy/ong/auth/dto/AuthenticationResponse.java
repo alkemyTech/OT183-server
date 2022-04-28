5c8f8d7c4ee90e7740519a51c727bf2110308d83
Matias Grando <matias.grando@hotmail.com>
2022-04-27 21:43:51 -0300

    Add jwt filter, user/login , generateToken

---
+++ src/main/java/com/alkemy/ong/auth/dto/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String jwt;
+}
