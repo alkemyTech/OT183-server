cede46aa2c3dd23b286d48dd632813d62bb806fd
Chelso Zabala <55297121+celsobrc@users.noreply.github.com>
2022-04-29 22:09:53 -0300

    Contact basic dto added

---
+++ src/main/java/com/alkemy/ong/dto/ContactBasicDto.java
@@ -0,0 +1,11 @@
+package com.alkemy.ong.dto;
+
+public class ContactBasicDto {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String message;
+    private String created;
+
+}
