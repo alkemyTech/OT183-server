f8344638d053e12eb3b0742e748a0db5578549fa
Chelso Zabala <55297121+celsobrc@users.noreply.github.com>
2022-04-29 22:29:44 -0300

    Contact service modified

--- src/main/java/com/alkemy/ong/service/IContactService.java
+++ src/main/java/com/alkemy/ong/service/IContactService.java
@@ -1,8 +1,12 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.ContactBasicDto;
 import com.alkemy.ong.dto.ContactDto;
+
+import java.util.List;
 
 public interface IContactService {
 
     void addContact(ContactDto contactDto);
+    List<ContactBasicDto> listContacts();
 }
