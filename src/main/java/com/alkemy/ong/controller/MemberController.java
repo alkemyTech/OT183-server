8a24e35b5eacbca33cbb4d11932a80a35691e1cc
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-04-30 20:34:33 -0300

    Method to get all added

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -4,28 +4,34 @@ import javax.validation.Valid;
 
 import com.alkemy.ong.dto.MemberDTO;
 import com.alkemy.ong.dto.response.PostMembersDTO;
-import com.alkemy.ong.service.impl.MemberServiceImpl;
+import com.alkemy.ong.service.IMemberService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController{
-    
-    private final MemberServiceImpl memberService;
+
+    @Autowired
+    private final IMemberService memberService;
 
     @PostMapping
     public ResponseEntity<PostMembersDTO> createMember(@Valid @RequestBody MemberDTO memberDTO){
         
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.createMember(memberDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemberDTO>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.returnList());
     }
 
 }
