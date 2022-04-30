635fd26060e0595188cabc385cc44284b965d808
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-04-30 20:39:57 -0300

    Method to get all and its implementation added

--- src/main/java/com/alkemy/ong/service/impl/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/MemberServiceImpl.java
@@ -2,24 +2,34 @@ package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.MemberDTO;
 import com.alkemy.ong.dto.response.PostMembersDTO;
-import com.alkemy.ong.mapper.MemberMapperPost;
+import com.alkemy.ong.exception.NullListException;
+import com.alkemy.ong.mapper.MemberMapper;
 import com.alkemy.ong.model.Member;
 import com.alkemy.ong.repository.MemberRepository;
 import com.alkemy.ong.service.IMemberService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Locale;
 
 @RequiredArgsConstructor
 @Service
 public class MemberServiceImpl implements IMemberService{
     private final MemberRepository memberRepository;
 
+    @Autowired
+    private MemberMapper memberMapper;
+    @Autowired
+    private MessageSource message;
+
+
     @Override
     public PostMembersDTO createMember(MemberDTO memberDTO){
-        MemberMapperPost member = new MemberMapperPost();
+        MemberMapper member = new MemberMapper();
 
         Member memberSaved = memberRepository.save(member.mapperMember(memberDTO));
 
@@ -30,5 +40,14 @@ public class MemberServiceImpl implements IMemberService{
         return response;
         
     }
-    
+
+    @Override
+    public List<MemberDTO> returnList() {
+        List<Member> entityList = memberRepository.findAll();
+        if (entityList.size() == 0) {
+            throw new NullListException(message.getMessage("error.null_list", null, Locale.US));
+        }
+        return memberMapper.listMemberDto(entityList);
+    }
+
 }
