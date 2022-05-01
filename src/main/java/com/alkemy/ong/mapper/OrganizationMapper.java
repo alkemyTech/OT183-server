35a8ca857471b9adbcef3893004cd172fb5d3269
mary-puma <pumaisabel029@gmail.com>
2022-05-01 11:37:26 -0300

    method mappingOrganizationDto added and the list of slides was sorted

--- src/main/java/com/alkemy/ong/mapper/OrganizationMapper.java
+++ src/main/java/com/alkemy/ong/mapper/OrganizationMapper.java
@@ -2,10 +2,13 @@ package com.alkemy.ong.mapper;
 
 import com.alkemy.ong.dto.OrganizationDto;
 import com.alkemy.ong.model.Organization;
+import com.alkemy.ong.model.Slide;
 import com.alkemy.ong.util.MapperUtil;
 import org.springframework.stereotype.Component;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class OrganizationMapper implements IMapper<Organization, OrganizationDto> {
@@ -69,4 +72,28 @@ public class OrganizationMapper implements IMapper<Organization, OrganizationDto
     public List<OrganizationDto> toDtoList(List<Organization> list) {
         return MapperUtil.streamListNonNull(list, this::toDto);
     }
+
+    public OrganizationDto mappingOrganizationDto(Organization entity) {
+        SlideMapper slideMapper = SlideMapper.getInstance();
+        return OrganizationDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .image(entity.getImage())
+                .address(entity.getAddress())
+                .phone(entity.getPhone())
+                .email(entity.getEmail())
+                .welcomeText(entity.getWelcomeText())
+                .aboutUsText(entity.getAboutUsText())
+                .created(entity.getCreated())
+                .updated(entity.getUpdated())
+                .slides(slideMapper
+                        .toDtoList(entity
+                                .getSlides()
+                                .stream()
+                                .sorted(Comparator.comparing(Slide::getPosition))
+                                .collect(Collectors.toList())))
+                .build();
+    }
+
+
 }
