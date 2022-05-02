275f0a788bb094f7a4ea53fa068b7748a9887a92
lucasbravi2019 <lucas.bravi.2008@gmail.com>
2022-05-02 20:26:46 -0300

    OT183-67 Service Testimonial controller delete method added

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -5,10 +5,7 @@ import com.alkemy.ong.service.impl.TestimonialServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,5 +21,10 @@ public class TestimonialController {
         return new ResponseEntity<>(service.createTestimonial(dto), HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteTestimonial(@PathVariable Long id) {
+        service.deleteTestimonial(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
