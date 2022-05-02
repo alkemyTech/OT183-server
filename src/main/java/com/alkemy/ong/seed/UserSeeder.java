8ef372d5a1b6d9041d854b65a2af81cefb0394c9
lucasbravi2019 <lucas.bravi.2008@gmail.com>
2022-05-02 16:00:33 -0300

    OT183-72 User Seeder refactor

--- src/main/java/com/alkemy/ong/seed/UserSeeder.java
+++ src/main/java/com/alkemy/ong/seed/UserSeeder.java
@@ -22,6 +22,52 @@ public class UserSeeder implements CommandLineRunner {
     private final PasswordEncoder passwordEncoder;
     private final MessageSource messageSource;
 
+    private final String[] names = {
+            "Adriana",
+            "Alejandro",
+            "Alexander",
+            "Andrea",
+            "Andres",
+            "Angela",
+            "Angelica",
+            "Angie",
+            "Camilo",
+            "Carlos",
+            "Carol",
+            "Carolina",
+            "Catherine",
+            "Cinthya",
+            "Claudia",
+            "Cristina",
+            "Daniel",
+            "Daniela",
+            "Diana",
+            "Diego"
+    };
+
+    private final String[] lastnames = {
+            "Hernandez",
+            "Sanchez",
+            "Acevedo",
+            "Vargas",
+            "Acero",
+            "Garcia",
+            "Monroy",
+            "Piñeros",
+            "Blanco",
+            "Fernandez",
+            "Ruiz",
+            "Rodriguez",
+            "Cortes",
+            "Gomez",
+            "Castellanos",
+            "Contreras",
+            "Pinzon",
+            "Alfonso",
+            "Guzman",
+            "Torres"
+    };
+
     private enum RoleType {
         ADMIN,
         USER
@@ -30,92 +76,86 @@ public class UserSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if (userRepository.getUsersQuantity() > 0) return;
-        String[] names = {
-                "Adriana",
-                "Alejandro",
-                "Alexander",
-                "Andrea",
-                "Andres",
-                "Angela",
-                "Angelica",
-                "Angie",
-                "Camilo",
-                "Carlos",
-                "Carol",
-                "Carolina",
-                "Catherine",
-                "Cinthya",
-                "Claudia",
-                "Cristina",
-                "Daniel",
-                "Daniela",
-                "Diana",
-                "Diego"
-        };
 
-        String[] lastnames = {
-                "Hernandez",
-                "Sanchez",
-                "Acevedo",
-                "Vargas",
-                "Acero",
-                "Garcia",
-                "Monroy",
-                "Piñeros",
-                "Blanco",
-                "Fernandez",
-                "Ruiz",
-                "Rodriguez",
-                "Cortes",
-                "Gomez",
-                "Castellanos",
-                "Contreras",
-                "Pinzon",
-                "Alfonso",
-                "Guzman",
-                "Torres"
-        };
+
 
         if (roleRepository.getRoleQuantity() == 0) {
-            roleRepository.save(
-                    Role.builder()
-                            .name(RoleType.USER.toString())
-                            .build()
-            );
-            roleRepository.save(
-                    Role.builder()
-                            .name(RoleType.ADMIN.toString())
-                            .build()
-            );
+            createRoles();
         }
 
-        Role userRole = roleRepository.findByName(RoleType.USER.toString())
-                .orElseThrow(() -> new RoleNotFoundException(
-                                messageSource.getMessage("error.role_not_found", null, Locale.US)
-                        )
-                );
+        Role userRole = getRole(RoleType.USER.toString());
 
-        Role adminRole = roleRepository.findByName(RoleType.ADMIN.toString())
-                .orElseThrow(() -> new RoleNotFoundException(
-                                messageSource.getMessage("error.role_not_found", null, Locale.US)
-                        )
-                );
+        Role adminRole = getRole(RoleType.ADMIN.toString());
 
         for (int i = 0; i < 20; i++) {
-            UserModel user = UserModel.builder()
-                    .firstName(names[i])
-                    .lastName(lastnames[i])
-                    .password(passwordEncoder.encode("123456789"))
-                    .email(names[i] + "." + lastnames[i] + "@gmail.com")
-                    .photo("/assets/photos/user" + (i + 1))
-                    .role(i < 10 ? userRole : adminRole)
-                    .comments(null)
-                    .build();
-
-            userRepository.save(user);
+            createUser(
+                    getFirstname(i),
+                    getLastname(i),
+                    getEmail(getFirstname(i), getLastname(i)),
+                    getPassword(),
+                    getPhoto(i),
+                    getRoleByIndex(i, userRole, adminRole)
+            );
         }
 
     }
 
+    public void createUser(String firstname, String lastname, String email, String password, String photo, Role role) {
+        UserModel user = UserModel.builder()
+                .firstName(firstname)
+                .lastName(lastname)
+                .password(password)
+                .email(email)
+                .photo(photo)
+                .role(role)
+                .build();
+        userRepository.save(user);
+    }
+
+    public void createRoles() {
+        roleRepository.save(
+                Role.builder()
+                        .name(RoleType.USER.toString())
+                        .build()
+        );
+        roleRepository.save(
+                Role.builder()
+                        .name(RoleType.ADMIN.toString())
+                        .build()
+        );
+    }
+
+    public Role getRole(String name) {
+        return roleRepository.findByName(name)
+                .orElseThrow(() -> new RoleNotFoundException(
+                                messageSource.getMessage("error.role_not_found", null, Locale.US)
+                        )
+                );
+    }
+
+    public String getPassword() {
+        return passwordEncoder.encode("123456789");
+    }
+
+    public String getPhoto(int id) {
+        return "/assets/photos/user" + (id + 1);
+    }
+
+    public String getFirstname(int index) {
+        return names[index];
+    }
+
+    public String getLastname(int index) {
+        return lastnames[index];
+    }
+
+    public Role getRoleByIndex(int index, Role userRole, Role adminRole) {
+        if (index < 10) return userRole;
+        return adminRole;
+    }
+
+    public String getEmail(String firstname, String lastname) {
+        return firstname.toLowerCase() + "." + lastname.toLowerCase() + "@gmail.com";
+    }
 
 }
