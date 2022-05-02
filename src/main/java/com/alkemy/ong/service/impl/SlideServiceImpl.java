fc9f7a40e67a46be26cd10dd3387f04ccf9bc240
ianklebold <ianstgo@gmail.com>
2022-05-01 22:22:47 -0300

    SLIDE - 000 - ADD ENDPOINT UPDATE TO SLIDES

--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -1,15 +1,23 @@
 package com.alkemy.ong.service.impl;
 
+import com.alkemy.ong.dto.SlidesUpdateDTO;
+import com.alkemy.ong.dto.response.UpdateSlidesDTO;
 import com.alkemy.ong.dto.type.SlideDtoType;
 import com.alkemy.ong.exception.EntityNotFoundException;
 import com.alkemy.ong.mapper.SlideMapper;
+import com.alkemy.ong.model.Organization;
+import com.alkemy.ong.model.Slide;
+import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.repository.SlideRepository;
 import com.alkemy.ong.service.ISlideService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -18,6 +26,7 @@ public class SlideServiceImpl implements ISlideService {
     private final SlideRepository repository;
     private final SlideMapper mapper;
     private final MessageSource messageSource;
+    private final OrganizationRepository organizationRepository;
 
     @Override
     public Object getSlideDetail(Long id) {
@@ -37,4 +46,45 @@ public class SlideServiceImpl implements ISlideService {
 
         repository.deleteById(id);
     }
+
+    @Override
+    public ResponseEntity<?> updateSlide(Long id, SlidesUpdateDTO slideUpdate) {
+        Slide slide = repository.getById(id);
+
+        if(slideUpdate.getText() != null) slide.setText(slideUpdate.getText());
+        if(slideUpdate.getPosition() != null) slide.setPosition(slideUpdate.getPosition());
+        if(slideUpdate.getImageUrl()!= null) slide.setImageUrl(slideUpdate.getImageUrl());
+
+        // CONTROL IF ORGANIZATION EXISTS
+        if(slideUpdate.getOrganization() != null){
+            if(controlExistsOrganization(slideUpdate,slide) == false)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(messageSource.getMessage("data.not.found", null, Locale.US));
+        }
+        repository.save(slide);
+        UpdateSlidesDTO response = new UpdateSlidesDTO();
+        response.setId(slide.getId());
+        response.setUrl("slides/"+slide.getId());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    /**
+     * 
+     * @param slideUpdate
+     * @param slide
+     * @return true if Organization exists. 
+     * @return false if Organization not exists.
+     */
+    private Boolean controlExistsOrganization(SlidesUpdateDTO slideUpdate,Slide slide){
+            Optional<Organization> organizationRequest = 
+            organizationRepository.findById(slideUpdate.getOrganization());
+            if(organizationRequest.isPresent()){
+                slide.setOrganization(organizationRequest.get());
+                return true;
+            }else{
+                return false;
+            }
+    }
+
+    
 }
