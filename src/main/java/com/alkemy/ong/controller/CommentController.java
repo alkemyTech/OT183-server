261c8faa826a9a5252a2d9376a77b19c5c22e2db
lucasbravi2019 <80082740+lucasbravi2019@users.noreply.github.com>
2022-05-03 19:01:38 -0300

    Feature/ot183-75 (#71)
    * OT183-75 Comment repository query added
    
    * OT183-75 CommentBasicDto created
    
    * OT183-75 Comment Service interface and implementation getAllComments method added
    
    * OT183-75 Comment controller getAllComments method added
    
    * OT183-75 Endpoint security added

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -1,16 +1,15 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.CommentBasicDto;
 import com.alkemy.ong.dto.CommentDto;
 import com.alkemy.ong.service.ICommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("comments")
@@ -18,6 +17,14 @@ public class CommentController {
 
     @Autowired
     ICommentService commentService;
+
+    @GetMapping
+    public ResponseEntity<List<CommentBasicDto>> getAllComments() {
+        return new ResponseEntity<>(
+                commentService.getAllComments(),
+                HttpStatus.OK
+        );
+    }
 
     @PostMapping
     public ResponseEntity<CommentDto> save(@Valid @RequestBody CommentDto comment){
