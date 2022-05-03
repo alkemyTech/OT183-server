c9b7d4bdfa6ff4df6a8ed65f97d347314256d936
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-05-03 07:47:05 -0300

    Controller added

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -1,10 +1,14 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.SlideRequestDto;
+import com.alkemy.ong.dto.SlideResponseDto;
 import com.alkemy.ong.service.impl.SlideServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @AllArgsConstructor
 @RestController
@@ -13,6 +17,10 @@ public class SlideController {
 
     private final SlideServiceImpl service;
 
+    @PostMapping
+    public ResponseEntity<SlideResponseDto> createSlide(@Valid @RequestBody SlideRequestDto slideRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createSlide(slideRequestDto));
+    }
     @GetMapping("/{id}")
     public ResponseEntity<Object> getSlideData(@PathVariable Long id) {
         return new ResponseEntity<>(service.getSlideDetail(id), HttpStatus.OK);
