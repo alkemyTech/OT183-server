193b1d9f2829e278de45ec50759b6925bd6c699f
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-05-03 08:16:18 -0300

    Method to save slide and its implementation + aux methods added

--- src/main/java/com/alkemy/ong/service/ISlideService.java
+++ src/main/java/com/alkemy/ong/service/ISlideService.java
@@ -1,9 +1,13 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.SlideRequestDto;
+import com.alkemy.ong.dto.SlideResponseDto;
+
 public interface ISlideService {
 
     Object getSlideDetail(Long id);
 
     void deleteSlice(Long id);
 
+    SlideResponseDto createSlide(SlideRequestDto slideRequestDto);
 }
--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -1,14 +1,26 @@
 package com.alkemy.ong.service.impl;
 
+import com.alkemy.ong.dto.OrganizationDto;
+import com.alkemy.ong.dto.SlideDto;
+import com.alkemy.ong.dto.SlideRequestDto;
+import com.alkemy.ong.dto.SlideResponseDto;
 import com.alkemy.ong.dto.type.SlideDtoType;
 import com.alkemy.ong.exception.EntityNotFoundException;
+import com.alkemy.ong.exception.NullListException;
 import com.alkemy.ong.mapper.SlideMapper;
+import com.alkemy.ong.model.Slide;
 import com.alkemy.ong.repository.SlideRepository;
+import com.alkemy.ong.service.AmazonS3Service;
+import com.alkemy.ong.service.IOrganizationService;
 import com.alkemy.ong.service.ISlideService;
+import com.alkemy.ong.util.CustomMultipart;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.Locale;
 
 @AllArgsConstructor
@@ -16,8 +28,16 @@ import java.util.Locale;
 public class SlideServiceImpl implements ISlideService {
 
     private final SlideRepository repository;
+
     private final SlideMapper mapper;
+
     private final MessageSource messageSource;
+
+    @Autowired
+    private IOrganizationService OrganizationService;
+
+    @Autowired
+    private AmazonS3Service aws3service;
 
     @Override
     public Object getSlideDetail(Long id) {
@@ -37,4 +57,57 @@ public class SlideServiceImpl implements ISlideService {
 
         repository.deleteById(id);
     }
+
+    @Override
+    public SlideResponseDto createSlide(SlideRequestDto requestDto){
+        OrganizationDto dtoOrg = OrganizationService.getOrganizationDto(requestDto.getOrganizationId());
+        checkPosition(requestDto);
+        manageImage(requestDto);
+
+        SlideDto slideDto = new SlideDto(requestDto.getImage(), requestDto.getText(), requestDto.getPosition(), dtoOrg);
+        Slide toSave = mapper.toEntity(slideDto, dtoOrg.getId());
+
+        Slide slide = repository.save(toSave);
+
+        return mapper.toDtoResponse(slide);
+    }
+
+    ///Auxiliary methods:
+    private void checkPosition(SlideRequestDto requestDto){
+        if(requestDto.getPosition() == null){
+            requestDto.setPosition(String.valueOf((repository.count()+1)));
+        }
+    }
+
+    private void manageImage(SlideRequestDto requestDto){
+        requestDto.setImage(aws3service.uploadFile(convertImage(requestDto.getImage())));
+    }
+
+    private CustomMultipart convertImage(String imagen){
+        //Remove "data:image/jpeg;base64", just to keep the bytes to decode
+        String trimmedEncodedImage = imagen.substring(imagen.indexOf(",") + 1);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(trimmedEncodedImage);
+
+        String fileName = "slide"+getExtension(imagen);
+
+        CustomMultipart customMultipartFile = new CustomMultipart(decodedBytes, fileName);
+
+        try {
+            customMultipartFile.transferTo(customMultipartFile.getFile());
+        } catch (IOException e) {
+            throw new NullListException(messageSource.getMessage("error.convert_image", null, Locale.US));
+        }
+
+        return customMultipartFile;
+    }
+
+    public String getExtension(String imagen){
+        Integer positionInitial = imagen.indexOf("/")+1;
+        Integer positionFinal = imagen.indexOf(";");
+
+        String extension = "."+imagen.substring(positionInitial, positionFinal);
+        return extension;
+    }
+
 }
