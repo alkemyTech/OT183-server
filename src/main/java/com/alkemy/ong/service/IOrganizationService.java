6647be12b0cdf2f26a95cff87cc6f0a4423bc00e
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-05-03 08:14:51 -0300

    Method to generate dto and its implementation added, necessaries for SlideServiceImpl

--- src/main/java/com/alkemy/ong/service/IOrganizationService.java
+++ src/main/java/com/alkemy/ong/service/IOrganizationService.java
@@ -1,7 +1,10 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.OrganizationDto;
+
 public interface IOrganizationService {
 
     public Object getOrganizationPublicData();
+    OrganizationDto getOrganizationDto(Long id);
 
 }
