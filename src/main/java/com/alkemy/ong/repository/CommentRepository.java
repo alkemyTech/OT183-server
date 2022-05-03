c4dd1b0a3027ce517982f820b201af06e7326961
lucasbravi2019 <lucas.bravi.2008@gmail.com>
2022-05-03 04:53:03 -0300

    OT183-75 Comment repository query added

--- src/main/java/com/alkemy/ong/repository/CommentRepository.java
+++ src/main/java/com/alkemy/ong/repository/CommentRepository.java
@@ -1,9 +1,17 @@
 package com.alkemy.ong.repository;
 
+import com.alkemy.ong.dto.CommentBasicDto;
 import com.alkemy.ong.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CommentRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query(value = "SELECT new com.alkemy.ong.dto.CommentBasicDto(body) FROM Comment ORDER BY created")
+    List<CommentBasicDto> getAllComments();
+
 }
