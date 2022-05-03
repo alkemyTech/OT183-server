3ba38045152ad35718aa8f3f73c05c17ca6e1139
lucasbravi2019 <lucas.bravi.2008@gmail.com>
2022-05-03 04:54:53 -0300

    OT183-75 CommentBasicDto created

---
+++ src/main/java/com/alkemy/ong/dto/CommentBasicDto.java
@@ -0,0 +1,14 @@
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CommentBasicDto {
+
+    private String body;
+
+}
