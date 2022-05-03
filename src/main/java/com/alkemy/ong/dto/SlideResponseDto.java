609d0ab3be9f309765412a79c244a0659cbb98eb
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-05-03 07:49:44 -0300

    Dto for response and request added

---
+++ src/main/java/com/alkemy/ong/dto/SlideResponseDto.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SlideResponseDto {
+    private Long id;
+    private String imageUrl;
+    private String text;
+    private String position;
+    private Long organizationId;
+    private String organizationName;
+}
