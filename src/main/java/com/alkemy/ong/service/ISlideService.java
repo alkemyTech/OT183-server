60104c0f956d39230098b176cc066ed2f7a4dc18
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-05-04 23:03:22 -0300

    Fixed DTOs (changed data type of position) + add import necessaries + fixed relation between serviceSlide and its implementation

--- src/main/java/com/alkemy/ong/service/ISlideService.java
+++ src/main/java/com/alkemy/ong/service/ISlideService.java
@@ -2,12 +2,19 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.SlideRequestDto;
 import com.alkemy.ong.dto.SlideResponseCreationDto;
+import com.alkemy.ong.dto.SlideResponseDto;
 import org.springframework.http.ResponseEntity;
 import com.alkemy.ong.dto.SlidesUpdateDTO;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 public interface ISlideService {
+    @Transactional
+    List<SlideResponseDto> getAll();
+
     SlideResponseCreationDto createSlide(SlideRequestDto slideRequestDto);
     Object getSlideDetail(Long id);
     ResponseEntity<?> updateSlide(Long id, SlidesUpdateDTO slideUpdate);
-    void deleteSlice(Long id);
+    void deleteSlide(Long id);
 }
